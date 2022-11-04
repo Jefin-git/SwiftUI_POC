a9746e2a5318ed11902e9936131e6c4389115b10
Jefin Abdul Jaleel <jefin.abduljaleel@cognizant.com>
2022-11-04 12:36:08 +0530

    initial project created

---
+++ SwiftUI_POC/SwiftUI_POC/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUI_POC
+//
+//  Created by Abdul Jaleel, Jefin (Cognizant) on 04/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_POC/SwiftUI_POC/SwiftUI_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_POCApp.swift
+//  SwiftUI_POC
+//
+//  Created by Abdul Jaleel, Jefin (Cognizant) on 04/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
